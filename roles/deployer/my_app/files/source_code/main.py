25b9d831c275f09651a5256b07abc8d34a8f1a12
Dmitry Putilin <dk@dobry-kot.ru>
2020-07-10 04:33:12 +0300

    new app

---
+++ roles/deployer/my_app/files/source_code/main.py
@@ -0,0 +1,14 @@
+from flask import json
+from flask import request
+from flask import Flask
+
+_app = Flask(__name__)
+
+@_app.route('/myapp/', methods=['GET'])
+def hello_world():
+    return "Hello World", 200
+
+
+if __name__ == '__main__':
+    _app.run(host = '0.0.0.0', port = 64500)
+
